c3d881f953f57b058c80b95838fb1cbffd45a119
Danix160 <tiziotipo18@gmail.com>
2026-01-03 10:25:34 +0100

    Create build.gradle.kts

---
+++ ToonItaliaProvider/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1 // Inizia da 1
+
+cloudstream {
+    description = "Archivio di Anime e Cartoni animati in italiano da ToonItalia.xyz"
+    authors = listOf("Danix") // Metti il tuo nome qui
+
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "Cartoon",
+        "Movie"
+    )
+
+    requiresResources = false
+    language = "it"
+
+    // Icona del sito ToonItalia
+    iconUrl = "https://toonitalia.xyz/wp-content/uploads/2023/08/cropped-Majintoon-192x192.jpg"
+}
