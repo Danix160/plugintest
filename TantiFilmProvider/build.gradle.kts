83ce7685601562300dbe14f3f93bc94baf59eb95
Danix160 <tiziotipo18@gmail.com>
2026-01-04 15:09:17 +0100

    Create build.gradle.kts

---
+++ TantiFilmProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    extra["prefix"] = "TantiFilm"
+    extra["displayName"] = "TantiFilm"
+    
+    version = 1
+    description = "Film e Serie TV"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "Cartoon")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://tanti-film.stream/templates/tantifilm/images/favicon.ico"
+}
