a8a0363451ecd5c0dff636d6d90b02ce7fc8afe6
Danix160 <tiziotipo18@gmail.com>
2026-01-05 18:21:28 +0100

    Update build.gradle.kts

--- TantiFilmProvider/build.gradle.kts
+++ TantiFilmProvider/build.gradle.kts
@@ -2,7 +2,7 @@ cloudstream {
     extra["prefix"] = "TantiFilm"
     extra["displayName"] = "TantiFilm"
     
-    version = 12
+    version = 13
     description = "Film e Serie TV"
     authors = listOf("Danix")
     
