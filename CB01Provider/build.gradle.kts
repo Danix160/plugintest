aef13c0d1e2bbfefff991dc6113d1bd620d8854f
Danix160 <tiziotipo18@gmail.com>
2026-01-05 21:51:59 +0100

    Create build.gradle.kts

---
+++ CB01Provider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    extra["prefix"] = "CineBlog"
+    extra["displayName"] = "CineBlog"
+    
+    version = 1
+    description = "CineBlog01"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://cb01.uno/apple-icon-180x180px.png"
+}
