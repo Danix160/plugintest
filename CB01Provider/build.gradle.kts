9ba0e744e6cfb8b0190cc91aa080b1d1c5cb4c5e
Danix160 <tiziotipo18@gmail.com>
2026-01-06 14:40:17 +0100

    Update build.gradle.kts

--- CB01Provider/build.gradle.kts
+++ CB01Provider/build.gradle.kts
@@ -2,7 +2,7 @@ cloudstream {
     extra["prefix"] = "CineBlog"
     extra["displayName"] = "CineBlog"
     
-    version = 12
+    version = 13
     description = "CineBlog01"
     authors = listOf("Danix")
     
