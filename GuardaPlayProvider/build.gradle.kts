7a8bbdb132e7f449b9192448e74ced664e8f1d7d
Danix160 <tiziotipo18@gmail.com>
2026-01-08 16:30:41 +0100

    Create build.gradle.kts

---
+++ GuardaPlayProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    extra["prefix"] = "GuardaPlay"
+    extra["displayName"] = "Guardaplay"
+    
+    version = 21
+    description = "Film di Guardaplay"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://guardaplay.bar/wp-content/uploads/2025/12/cropped-guardaplaym.png"
+}
