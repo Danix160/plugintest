f8ffa0d44af06d5cdc9c9517383ceba500d7b55d
Danix160 <tiziotipo18@gmail.com>
2026-01-09 17:54:01 +0100

    Create build.gradle.kts

---
+++ CBProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    extra["prefix"] = "CB"
+    extra["displayName"] = "CB"
+    
+    version = 1
+    description = "CB001.uno"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://cb001.uno/templates/CB01_bid/images/logocb2-1.jpg"
+}
