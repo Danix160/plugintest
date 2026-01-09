5c2868e863c147d447b2788adf2cde3a75fc78eb
Danix160 <tiziotipo18@gmail.com>
2026-01-09 16:47:28 +0100

    Update build.gradle.kts

--- FilmezProvider/build.gradle.kts
+++ FilmezProvider/build.gradle.kts
@@ -7,7 +7,7 @@ cloudstream {
     authors = listOf("Danix")
     
     status = 1
-    tvTypes = listOf("Movie", "TvSeries")
+    tvTypes = listOf("Movie")
     requiresResources = false
     language = "it"
     iconUrl = "https://filmez.org/wp-content/uploads/2022/11/cropped-logo_filmez2.jpg"
