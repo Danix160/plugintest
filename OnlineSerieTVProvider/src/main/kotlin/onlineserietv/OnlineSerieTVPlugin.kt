2b6719185a37d30eb8e291c740926bca95d04052
Danix160 <tiziotipo18@gmail.com>
2026-01-27 10:08:04 +0100

    Create OnlineSerieTVPlugin.kt

---
+++ OnlineSerieTVProvider/src/main/kotlin/onlineserietv/OnlineSerieTVPlugin.kt
@@ -0,0 +1,13 @@
+package com.toonitalia // Deve essere uguale a quello in cima al file
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class OnlineSerieTVPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Registra il provider definito sopra nella classe ToonItaliaProvider
+        registerMainAPI(OnlineSerieTVProvider())
+    }
+}
