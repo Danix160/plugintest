1b35099293b897d668c58e4cc78242d5f899ea30
Danix160 <tiziotipo18@gmail.com>
2026-01-27 09:54:22 +0100

    Create build.gradle.kts

---
+++ OnlineSerieTVProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    extra["prefix"] = "OnlineSerieTV"
+    extra["displayName"] = "OnlineSerieTV"
+    
+    version = 1
+    description = "OnlineSerietv.com/Prova"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "Cartoon")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://onlineserietv.online/images/logo.svg"
+}
