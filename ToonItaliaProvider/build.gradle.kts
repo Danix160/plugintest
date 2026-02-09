99313b3bbe671f12d14d822fd66e7f6ebd29be3f
Danix160 <tiziotipo18@gmail.com>
2026-02-09 05:48:46 +0100

    Bump version from 30 to 31 in build.gradle.kts

--- ToonItaliaProvider/build.gradle.kts
+++ ToonItaliaProvider/build.gradle.kts
@@ -11,7 +11,7 @@ cloudstream {
     extra["prefix"] = "ToonItalia"
     extra["displayName"] = "ToonItalia"
     
-    version = 30
+    version = 31
     description = "Archivio di Anime e Cartoni animati in italiano da ToonItalia.xyz"
     authors = listOf("Danix")
     
