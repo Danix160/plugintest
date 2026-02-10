db05e18a3a0929f0cba928e5284d8cf3b8c95cd2
Danix160 <tiziotipo18@gmail.com>
2026-02-10 10:46:11 +0100

    Create build.gradle.kts

---
+++ AltadefinizioneProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "AltaDefinizione"
+    extra["displayName"] = "AltaDefinizione"
+    
+    version = 1
+    description = "AltaDefinizionez"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = list0f("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = ""
+}
