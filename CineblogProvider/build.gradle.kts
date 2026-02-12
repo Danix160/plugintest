6e42ca2378163b61a06aab9e3f15518c65fded0a
Danix160 <tiziotipo18@gmail.com>
2026-02-12 18:39:17 +0100

    Create build.gradle.kts

---
+++ CineblogProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "Cineblog"
+    extra["displayName"] = "Cineblog"
+    
+    version = 41
+    description = "cineblog001.club"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://cineblog001.club/templates/CB01/img/logo.png"
+}
