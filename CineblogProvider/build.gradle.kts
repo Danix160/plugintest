9c7f0106ff25fb84cd46684811228a25acc7dc0c
Danix160 <tiziotipo18@gmail.com>
2026-02-13 18:52:56 +0100

    Update build.gradle.kts

--- CineblogProvider/build.gradle.kts
+++ CineblogProvider/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     }
 }
 dependencies {
-    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
 cloudstream {
     extra["prefix"] = "Cineblog"
