931b46b97bc2625cc287e5eaccf9e129693f941b
Danix160 <tiziotipo18@gmail.com>
2026-02-14 16:44:18 +0100

    Create build.gradle.kts

---
+++ CbProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "CB01"
+    extra["displayName"] = "CB01"
+    
+    version = 1
+    description = "cb01uno.one"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://cb01uno.one/wp-content/uploads/2026/01/logo-official-uno-2026.png"
+}
