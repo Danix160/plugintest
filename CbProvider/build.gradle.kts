93f271eb389a729070511eb27692d1be843be6e1
Danix160 <tiziotipo18@gmail.com>
2026-02-14 20:50:11 +0100

    Update build.gradle.kts

--- CbProvider/build.gradle.kts
+++ CbProvider/build.gradle.kts
@@ -11,7 +11,7 @@ cloudstream {
     extra["prefix"] = "CB01"
     extra["displayName"] = "CB01"
     
-    version = 16
+    version = 17
     description = "cb01uno.one"
     authors = listOf("Danix")
     
