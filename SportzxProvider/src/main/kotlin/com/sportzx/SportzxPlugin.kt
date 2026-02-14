273e337a4121cd20499d087c288a0e17251a95a6
Danix160 <tiziotipo18@gmail.com>
2026-02-14 23:07:21 +0100

    Create SportzxPlugin.kt

---
+++ SportzxProvider/src/main/kotlin/com/sportzx/SportzxPlugin.kt
@@ -0,0 +1,13 @@
+package com.sportzx // Deve essere uguale a quello in cima al file
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SportzxPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Registra il provider definito sopra nella classe ToonItaliaProvider
+        registerMainAPI(SportzxProvider())
+    }
+}
