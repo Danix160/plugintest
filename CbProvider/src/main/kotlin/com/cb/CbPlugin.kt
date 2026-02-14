51e86681b48577dc3c9069669ccbd1a941375ec4
Danix160 <tiziotipo18@gmail.com>
2026-02-14 16:45:18 +0100

    Create CbPlugin.kt

---
+++ CbProvider/src/main/kotlin/com/cb/CbPlugin.kt
@@ -0,0 +1,13 @@
+package com.cb // Deve essere uguale a quello in cima al file
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class CbPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Registra il provider definito sopra nella classe ToonItaliaProvider
+        registerMainAPI(CbProvider())
+    }
+}
