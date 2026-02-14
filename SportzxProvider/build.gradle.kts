c725841e9e38c46f2c6154b9ffc44d7900d87aac
Danix160 <tiziotipo18@gmail.com>
2026-02-14 23:06:21 +0100

    Create build.gradle.kts

---
+++ SportzxProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "Sportzx"
+    extra["displayName"] = "Sportzx"
+    
+    version = 17
+    description = "cb01uno.one"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Live")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://sportzx.cc/wp-content/uploads/2025/10/sportzx.webp"
+}
