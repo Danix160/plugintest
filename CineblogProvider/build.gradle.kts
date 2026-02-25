a26fde52a347fe406040e8d511878699730404ab
Danix160 <tiziotipo18@gmail.com>
2026-02-25 02:59:07 +0100

    Update version from 58 to 59 in build.gradle.kts

--- CineblogProvider/build.gradle.kts
+++ CineblogProvider/build.gradle.kts
@@ -14,7 +14,7 @@ cloudstream {
     extra["prefix"] = "Cineblog"
     extra["displayName"] = "Cineblog"
     
-    version = 58
+    version = 59
     description = "cineblog001.club"
     authors = listOf("Danix")
     
