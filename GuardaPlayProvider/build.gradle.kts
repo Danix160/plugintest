ab316fc925c465608b375b5bbf6e0b5403cd3e4e
Danix160 <tiziotipo18@gmail.com>
2026-02-27 19:59:55 +0100

    Create build.gradle.kts

---
+++ GuardaPlayProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+}
+cloudstream {
+    extra["prefix"] = "GuardaPlay"
+    extra["displayName"] = "GuardaPlay"
+    
+    version = 1
+    description = "GuardaPlay Contiene Solo Film"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://guardaplay.space/wp-content/uploads/2025/12/cropped-guardaplaym.png"
+}
