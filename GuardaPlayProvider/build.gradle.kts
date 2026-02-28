41c4f37402eaf6e50aff8408aa1adacfe1db3376
Danix160 <tiziotipo18@gmail.com>
2026-02-28 02:09:06 +0100

    Update version from 9 to 10 in build.gradle.kts

--- GuardaPlayProvider/build.gradle.kts
+++ GuardaPlayProvider/build.gradle.kts
@@ -14,7 +14,7 @@ cloudstream {
     extra["prefix"] = "GuardaPlay"
     extra["displayName"] = "GuardaPlay"
     
-    version = 9
+    version = 10
     description = "GuardaPlay Contiene Solo Film"
     authors = listOf("Danix")
     
